d4b8520e60219f28c620582e6a04f191db9b3636
ahmed04raxa <ahmed04raxa@gmail.com>
2025-12-05 08:26:27 +0500

    Lecture_2

---
+++ Lecture_2/Lecture_2.cpp
@@ -0,0 +1,44 @@
+/*
+    BOILER PLATE CODE
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    return 0;
+}
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int age = 25;
+    // char grade = 'A';
+    // float PI = 3.14f;
+    // bool isSafe = false;
+    // double is = 90;
+    // double price =100.0;
+    // int newPrice = price;
+    // cout << newPrice;
+
+    // int num1;
+    // cout << "Enter Your First Number : ";
+    // cin >> num1;
+    // int num2;
+    // cout << "Enter Your Second Number : ";
+    // cin >> num2;
+    // int sum = num1 + num2;
+    // cout << "The Sum Your Numbers Is " << sum << endl;
+
+    int num1, num2;
+    cout << "Enter Value oF Num1 : ";
+    cin >> num1;
+    cout << "Enter Value oF Num2 : ";
+    cin >> num2;
+
+    int sum = num1 + num2;
+    cout << "Sum Of Num1 && Num2 Is : " << sum;
+}
