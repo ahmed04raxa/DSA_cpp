fe714771c7af76746bbc55b2c3ba27d66a711f8c
ahmed04raxa <ahmed04raxa@gmail.com>
2025-12-29 08:25:37 +0500

    Lecture_3

---
+++ Lecture_3/Lecture_3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int n = 5;
+    // if (n >= 0)
+    // {
+    //     cout << "N Is Positive";
+    // }
+    // else
+    // {
+    //     cout << "N Is Negative";
+    // }
+
+    // for (int i = 0; i <= 5; i++)
+    // {
+    //     cout << "Hello World" <<" " <<i << endl;
+    // }
+    // return 0;
+}
